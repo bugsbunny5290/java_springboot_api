45f3632c5fc1e54911307b993c413d0656933066
Pranav Gautam <pranav.gautam.pro@gmail.com>
2019-09-07 20:59:57 +0200

    implemented further actions
    connected to postgresql

--- src/main/java/com/example/simple_api/dao/PersonDataAccessService.java
+++ src/main/java/com/example/simple_api/dao/PersonDataAccessService.java
@@ -5,40 +5,64 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
-@Repository("pDao")
+@Repository("postgres")
 public class PersonDataAccessService implements PersonDao {
+
+  private final JdbcTemplate jdbcTemplate;
+
 
   private static List<Person> DB = new ArrayList<>();
 
+
+  @Autowired
+  public PersonDataAccessService(JdbcTemplate jdbcTemplate) {
+    this.jdbcTemplate = jdbcTemplate;
+  }
+
   @Override
   public int insertPerson(UUID id, Person person) {
+
+    final String sql = "INSERT INTO person (id, name) VALUES (?,?)";
+
+//    return jdbcTemplate.update(sql);
+
     DB.add(new Person(id, person.getName()));
     return 1;
   }
 
   @Override
   public List<Person> selectAllPeople() {
-    return DB;
+    final String sql = "SELECT id, name FROM person";
+    return jdbcTemplate.query(sql, (resultSet, i) -> {
+      UUID id = UUID.fromString(resultSet.getString("id"));
+      String name = resultSet.getString("name");
+      return new Person(id, name);
+    });
   }
 
   @Override
   public Optional<Person> selectPersonById(UUID id) {
-    return DB.stream()
-        .filter(person -> person.getId().equals(id))
-        .findFirst();
+    final String sql = "SELECT id, name FROM person WHERE id = ?";
+    Person person = jdbcTemplate.queryForObject(sql, new Object[]{id}, (resultSet, i) -> {
+      UUID personId = UUID.fromString(resultSet.getString("id"));
+      String name = resultSet.getString("name");
+      return new Person(personId, name);
+    });
+    return Optional.ofNullable(person);
   }
 
   @Override
   public int deletePersonById(UUID id) {
-    Optional<Person> personCheck = selectPersonById(id);
 
-    if (personCheck.isEmpty()) {
-      return 0;
-    }
-    DB.remove(personCheck.get());
-    return 1;
+    final String sql = "DELETE * FROM person WHERE id = ?";
+
+    return jdbcTemplate.update(sql, id);
+
   }
 
   @Override
